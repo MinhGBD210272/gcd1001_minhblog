843f2e2d0ea999aabdb383c6cbf63891439671f0
MinhGBD210272 <minhhxgbd210272@fpt.edu.vn>
2022-06-30 13:49:55 +0700

    new

---
+++ src/Controller/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_post")
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+    /**
+     * @Route ("/post/{id}", name="post_show")
+     * @return Response
+     */
+    public function show(Request $request, PostRepository $postRepository)
+    {
+        $postId = $request->attributes->get('id');
+        $post = $postRepository->find($postId);
+        return $this->render('post/show.html.twig', [
+            'post'=>$post
+        ]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('en_US');
+
+        for($i=1; $i<=5; $i++){
+            $category = new Category();
+            $category->setTitle($faker->word);
+            $manager->persist($category);
+            //Add post
+            for($j=1; $j <= 20; $j++){
+                $post = new Post();
+                $post->setTitle($faker->sentence(3));
+                $post->setContent($faker->text(500));
+                $post->setCreatedAt($faker->dateTimeBetween('- 3 months'));
+                $post->setCategory($category);
+                $manager->persist($post);
+            }
+        }
+
+
+
+            $manager->flush();
+
+    }
+}
